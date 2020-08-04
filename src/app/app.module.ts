8f4fceda010397fc68160a9e7d1c14e1d455f0bb
Oscar Martinez <oscarivanmtz3@gmail.com>
2020-08-04 10:14:04 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule , LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectorComponent } from './components/selector/selector.component';
+import { AccountComponent } from './components/account/account.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import es from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(es);
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    SelectorComponent,
+    AccountComponent,
+    NavbarComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es-MX' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/selector/selector.component.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbDateStruct, NgbTimeStruct, NgbTimepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.css'],
+  providers: [NgbTimepickerConfig]
+})
+export class SelectorComponent implements OnInit {
+
+  model: NgbDateStruct;
+  meridian = true;
+
+  cuenta: string;
+  cuentaP: string;
+  fa: string;
+  faP: string;
+  type: string;
+  typeP: string;
+  animation: string;
+  animationP: string;
+  description = false;
+  movementIncome: any[] = [];
+
+  incomeForm: FormGroup;
+
+  time: NgbTimeStruct = {hour: 13, minute: 30, second: 0};
+
+  constructor(private config: NgbTimepickerConfig,
+              private fb: FormBuilder,
+              private validadores: ValidadoresService) {
+
+    // customize default values of ratings used by this component tree
+    config.seconds = false;
+    config.spinners = true;
+    config.meridian = true;
+
+    this.createForm();
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  get cantidadNoValida(){
+    return this.incomeForm.get('cantidad').invalid && this.incomeForm.get('cantidad').touched;
+  }
+
+  get cuentaNoValida() {
+    return this.incomeForm.get('cuenta').invalid && this.incomeForm.get('cuenta').touched;
+  }
+
+  get categoriaNoValida(){
+    return this.incomeForm.get('categoria').invalid && this.incomeForm.get('cantidad').touched;
+  }
+
+  get fechaNoValida(){
+    return this.incomeForm.get('fecha').invalid && this.incomeForm.get('fecha').touched;
+  }
+
+  get horaNoValida(){
+    return this.incomeForm.get('hora').invalid && this.incomeForm.get('hora').touched;
+  }
+
+  selected(text, fa, type){
+    this.cuenta = text;
+    this.fa = fa;
+    this.type = type;
+    this.animation = 'animate__fadeIn';
+    this.cargarIncome();
+  }
+
+  selectedPeriodo(text, fa, type){
+    this.cuentaP = text;
+    this.faP = fa;
+    this.typeP = type;
+    this.animationP = 'animate__fadeIn';
+  }
+
+  removeClass(){
+    this.animation = '';
+    this.animationP = '';
+  }
+
+  saveIncome() {
+    if (this.incomeForm.invalid){
+      return this.incomeForm.markAllAsTouched();
+    }
+    console.log(this.incomeForm);
+    this.movementIncome.push(this.incomeForm.value);
+    console.log(this.movementIncome);
+  }
+
+  createForm() {
+    this.incomeForm = this.fb.group({
+      cantidad : ['', [Validators.required]],
+      cuenta : [ '', [Validators.required]],
+      categoria : [ '' , [Validators.required]],
+      descripccion : [ ],
+      fecha : ['', [Validators.required]],
+      hora : ['', [Validators.required]],
+    });
+  }
+
+  cargarIncome() {
+    this.incomeForm.reset({
+    cantidad: '1500',
+    cuenta: this.cuenta,
+    categoria: 'Sueldo',
+    descripccion: 'descripccion corta',
+  });
+}
+
+
+}
